60ab5cd198e21ef10fc354b5cf1fdc5a39e7e664
FarMakay <farischamakay09@gmail.com>
2025-06-17 14:58:52 +0700

    tes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/farischa-makay" : "/",
+  base: mode === "production" ? "/farischa-makay/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
