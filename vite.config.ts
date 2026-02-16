f63a9c02ea28f5def77320eca9a181e8a59b912a
ita <farischamakay09@gmail.com>
2026-02-16 16:09:26 +0700

    fix: remove base path for correct vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/farischa-makay/", // Sesuai dengan nama repository GitHub kamu
   plugins: [react()],
   resolve: {
     alias: {
